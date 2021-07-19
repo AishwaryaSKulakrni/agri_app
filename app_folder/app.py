8f8bb80930a7e80832fe57de5bce450d414590ca
AishwaryaSKulakrni <56063686+AishwaryaSKulakrni@users.noreply.github.com>
2021-07-19 08:49:06 +0530

    app

--- app_folder/app.py
+++ app_folder/app.py
@@ -1,13 +1,39 @@
 import streamlit as st
-from multiapp import MultiApp
-from app import home, explore # import your app modules here
+import pickle
+import numpy as np
 
-app = MultiApp()
+soilmodel=pickle.load(open('soilclass.pkl','rb'))
+crop=pickle.load(open('crop.pkl','rb'))
 
-st.title("Soil Classification, Crop Prediction and Fertilizer recommendation using Deep Learning Apporach")
+def soil_classification(n,p,k,ph):
+    input=np.array([[n,p,k,ph]]).astype(np.float64)
+    prediction=soilmodel.predict(input)
+    pred= (f"{prediction.item().title()}")
+    return str(pred)
 
-# Add all your application here
-app.add_app("Home", home.app)
-app.add_app("Data", explore.app)
-# The main app
-app.run()
+def crop_prediction(temp,humid,rain):
+    input=np.array([[temp,humid,rain]]).astype(np.float64)
+    prediction=crop.predict(input)
+    pred= (f"{prediction.item().title()}")
+    return str(pred)
+
+def main():
+    st.title("Soil Classfication")
+
+    n = st.text_input('Nitrogen')
+    p = st.text_input('Phosphorous')
+    k = st.text_input('Potassium')
+    ph = st.text_input('PH')
+    temp = st.text_input('Temperature')
+    humid = st.text_input('Humidity')
+    rain = st.text_input('Rainfall')
+
+    output=soil_classification(n,p,k,ph)
+    crop_op=crop_prediction(temp,humid,rain)
+
+    if st.button("SUBMIT"):
+        st.success('Soil nature for given soil nutrients is: {}'.format(output))
+        st.success('Crop grown for given soil nutrients is: {}'.format(crop_op))
+
+if __name__=='__main__':
+    main()
